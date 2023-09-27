059d4cb19639b871ce276499b701234e9df43a87
SoudruhRomanCZ <soudruhromancz@gmail.com>
2023-09-27 20:29:24 +0200

    added gyroscope ultrasound and joystick code

--- Bakalarka/src/main.cpp
+++ Bakalarka/src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
-
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+#include "SRF05.h"
 
 //define the Arduino pins configuration
 #define WIFI_RX_PIN 0
 #define WIFI_TX_PIN 1
-#define POT_PIN 2
+#define POT_PIN A3
 #define ENC_CLK_PIN 3
 #define ENC_DT_PIN 4
 #define ENC_SW_PIN 5
@@ -13,25 +16,235 @@
 #define SW1_PIN 8
 #define SW2_PIN 9
 #define BUZ_PIN 10
-#define MIC_PIN A0
+#define pinX A0
+#define pinY A1
+#define pinKey 2
+//#define MIC_PIN A0
 // #define IO_expander A1 A2 
+
+int nulaX, nulaY;
 
 #define GYR_SDA_PIN A4
 #define GYR_SCLK_PIN A5
 
-// put function declarations here:
-int myFunction(int, int);
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+
+const char LBRACKET = '[';
+const char RBRACKET = ']';
+const char COMMA    = ',';
+const char BLANK    = ' ';
+const char PERIOD   = '.';
+
+const int iAx = 0;
+const int iAy = 1;
+const int iAz = 2;
+const int iGx = 3;
+const int iGy = 4;
+const int iGz = 5;
+
+const int usDelay = 3150;   // empirical, to hold sampling to 200 Hz
+const int NFast =  1000;    // the bigger, the better (but slower)
+const int NSlow = 10000;    // ..
+const int LinesBetweenHeaders = 5;
+      int LowValue[6];
+      int HighValue[6];
+      int Smoothed[6];
+      int LowOffset[6];
+      int HighOffset[6];
+      int Target[6];
+      int LinesOut;
+      int N;
+
+// ultrazvuk
+const int trigger = 7;
+const int echo    = 6;
+SRF05 SRF(trigger, echo);
+// 
+
+
+void ForceHeader()
+  { LinesOut = 99; }
+    
+void GetSmoothed()
+  { int16_t RawValue[6];
+    int i;
+    long Sums[6];
+    for (i = iAx; i <= iGz; i++)
+      { Sums[i] = 0; }
+//    unsigned long Start = micros();
+
+    for (i = 1; i <= N; i++)
+      { // get sums
+        accelgyro.getMotion6(&RawValue[iAx], &RawValue[iAy], &RawValue[iAz], 
+                             &RawValue[iGx], &RawValue[iGy], &RawValue[iGz]);
+        if ((i % 500) == 0)
+          Serial.print(PERIOD);
+        delayMicroseconds(usDelay);
+        for (int j = iAx; j <= iGz; j++)
+          Sums[j] = Sums[j] + RawValue[j];
+      } // get sums
+//    unsigned long usForN = micros() - Start;
+//    Serial.print(" reading at ");
+//    Serial.print(1000000/((usForN+N/2)/N));
+//    Serial.println(" Hz");
+    for (i = iAx; i <= iGz; i++)
+      { Smoothed[i] = (Sums[i] + N/2) / N ; }
+  } // GetSmoothed
+
+void Initialize()
+  {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(9600);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    Serial.println("PID tuning Each Dot = 100 readings");
+  /*A tidbit on how PID (PI actually) tuning works. 
+    When we change the offset in the MPU6050 we can get instant results. This allows us to use Proportional and 
+    integral of the PID to discover the ideal offsets. Integral is the key to discovering these offsets, Integral 
+    uses the error from set-point (set-point is zero), it takes a fraction of this error (error * ki) and adds it 
+    to the integral value. Each reading narrows the error down to the desired offset. The greater the error from 
+    set-point, the more we adjust the integral value. The proportional does its part by hiding the noise from the 
+    integral math. The Derivative is not used because of the noise and because the sensor is stationary. With the 
+    noise removed the integral value lands on a solid offset after just 600 readings. At the end of each set of 100 
+    readings, the integral value is used for the actual offsets and the last proportional reading is ignored due to 
+    the fact it reacts to any noise.
+  */
+        accelgyro.CalibrateAccel(6);
+        accelgyro.CalibrateGyro(6);
+        Serial.println("\nat 600 Readings");
+        accelgyro.PrintActiveOffsets();
+        Serial.println();
+        accelgyro.CalibrateAccel(1);
+        accelgyro.CalibrateGyro(1);
+        Serial.println("700 Total Readings");
+        accelgyro.PrintActiveOffsets();
+        Serial.println();
+        accelgyro.CalibrateAccel(1);
+        accelgyro.CalibrateGyro(1);
+        Serial.println("800 Total Readings");
+        accelgyro.PrintActiveOffsets();
+        Serial.println();
+        accelgyro.CalibrateAccel(1);
+        accelgyro.CalibrateGyro(1);
+        Serial.println("900 Total Readings");
+        accelgyro.PrintActiveOffsets();
+        Serial.println();    
+        accelgyro.CalibrateAccel(1);
+        accelgyro.CalibrateGyro(1);
+        Serial.println("1000 Total Readings");
+        accelgyro.PrintActiveOffsets();
+     Serial.println("\n\n Any of the above offsets will work nice \n\n Lets proof the PID tuning using another method:"); 
+  } // Initialize
+
+void SetOffsets(int TheOffsets[6])
+  { accelgyro.setXAccelOffset(TheOffsets [iAx]);
+    accelgyro.setYAccelOffset(TheOffsets [iAy]);
+    accelgyro.setZAccelOffset(TheOffsets [iAz]);
+    accelgyro.setXGyroOffset (TheOffsets [iGx]);
+    accelgyro.setYGyroOffset (TheOffsets [iGy]);
+    accelgyro.setZGyroOffset (TheOffsets [iGz]);
+  } // SetOffsets
+
+void ShowProgress()
+  { if (LinesOut >= LinesBetweenHeaders)
+      { // show header
+        Serial.println("\tXAccel\t\t\tYAccel\t\t\t\tZAccel\t\t\tXGyro\t\t\tYGyro\t\t\tZGyro");
+        LinesOut = 0;
+      } // show header
+    Serial.print(BLANK);
+    for (int i = iAx; i <= iGz; i++)
+      { Serial.print(LBRACKET);
+        Serial.print(LowOffset[i]),
+        Serial.print(COMMA);
+        Serial.print(HighOffset[i]);
+        Serial.print("] --> [");
+        Serial.print(LowValue[i]);
+        Serial.print(COMMA);
+        Serial.print(HighValue[i]);
+        if (i == iGz)
+          { Serial.println(RBRACKET); }
+        else
+          { Serial.print("]\t"); }
+      }
+    LinesOut++;
+  } // ShowProgress
+
+
+void SetAveraging(int NewN)
+  { N = NewN;
+    Serial.print("averaging ");
+    Serial.print(N);
+    Serial.println(" readings each time");
+   } // SetAveraging
+
+void setup()
+  { Initialize();
+    for (int i = iAx; i <= iGz; i++)
+      { // set targets and initial guesses
+        Target[i] = 0; // must fix for ZAccel 
+        HighOffset[i] = 0;
+        LowOffset[i] = 0;
+      } // set targets and initial guesses
+    Target[iAz] = 16384;
+    SetAveraging(NFast);
+    Serial.println("-------------- done --------------");
+  
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //potenciometr
+  pinMode(POT_PIN, INPUT);
+  analogRead(POT_PIN);
+  //
+
+  // inicializace tlačítka joysticku
+  pinMode(pinKey, INPUT_PULLUP);
+  // komunikace přes sériovou linku rychlostí 9600 baud
+  Serial.begin(9600);
+  // načtení a uložení hodnot pro x a y osy
+  nulaX = analogRead(pinX);
+  nulaY = analogRead(pinY);
+  // 
+
+  //ultrazvuk
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+
+  SRF.setCorrectionFactor(1.035);
+  SRF.setModeAverage(10);
+  //
+  } // setup
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // System Disabled
+  if (digitalRead(A0) != 0)
+  {
+    // Turn indicator light off.
+    digitalWrite(LED_BUILTIN, 0);
+    return;
+  }
+
+  // Turn indicator light on.
+  digitalWrite(LED_BUILTIN, 1);
+  Serial.println(SRF.getMillimeter());
+  delay(100);
 }
